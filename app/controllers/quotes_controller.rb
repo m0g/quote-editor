6e22bfd1a8ebde21dacb578c27ceaa40dbc0afdc
Loïc Nogues <nogues.loic@gmail.com>
2023-02-03 18:40:08 +0100

    Turbo frames quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -28,7 +28,10 @@ class QuotesController < ApplicationController
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream
+      end
     else
       # Add `status: :unprocessable_entity` here
       render :edit, status: :unprocessable_entity
@@ -37,7 +40,11 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream
+    end
   end
 
   private
